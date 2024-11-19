e8981eea8ffab0df5b3e80e9eb0dc691565d75ea
sheikharbab <arbabzafar4444@gmail.com>
2024-11-19 19:53:10 +0500

    update project page

--- src/app/(user)/our-work/page.tsx
+++ src/app/(user)/our-work/page.tsx
@@ -1,16 +1,22 @@
-import * as C from "@/components/index";
+// import * as C from "@/components/index";
 import { companyName } from "@/constants";
 
 const Work = () => (
   <>
-    <C.Hero
+    {/* <C.Hero
       url="#contact-form"
       btn={"tell us about your project"}
       heading="Unveiling Our Diverse Portfolio of Success Stories"
       poster="/banner-1.png"
     />
 
-    <C.WorkInfo />
+    <C.WorkInfo /> */}
+    <div className="flex items-center justify-between bg-primary pt-16">
+      
+  
+  <img src="/under.jpg" className="w-full h-full object-contain" alt="" width="" height=""/> 
+
+    </div>
   </>
 );
 
